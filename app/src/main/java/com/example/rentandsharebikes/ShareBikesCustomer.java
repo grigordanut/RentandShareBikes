17ab3c0ba5da6af76130c0dd6f06118265526bfd
grigordanut <danut.grigor@gmail.com>
2020-04-28 15:23:54 +0100

    Add share bikes functionlity

---
+++ app/src/main/java/com/example/rentandsharebikes/ShareBikesCustomer.java
@@ -0,0 +1,14 @@
+package com.example.rentandsharebikes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShareBikesCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_bikes_customer);
+    }
+}
