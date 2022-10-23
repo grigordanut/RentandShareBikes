3ac3c2231677570dae876c3c3234cd5caabca5df
Grigor Danut <grigor@seamlesscare.ie>
2022-10-23 04:57:52 +0100

    Updated Scanner feature.

---
+++ app/src/main/java/com/example/rentandsharebikes/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.example.rentandsharebikes;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/example/rentandsharebikes/Scanner.java
+++ app/src/main/java/com/example/rentandsharebikes/Scanner.java
@@ -1,74 +1,64 @@
 package com.example.rentandsharebikes;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class Scanner extends AppCompatActivity {
 
-//    private static final int PERMISSION_CODE = 1000;
-//    CodeScanner codeScanner;
-//    CodeScannerView scannerView;
-//    TextView resultData;
+    private ImageButton buttonOpenScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
 
-//        scannerView = findViewById(R.id.scannerView);
-//        codeScanner = new CodeScanner(this,scannerView);
-//        resultData = findViewById(R.id.resultsOfQr);
-//        codeScanner.setDecodeCallback(new DecodeCallback() {
-//            @Override
-//            public void onDecoded(@NonNull final Result result) {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        //resultData.setText(result.getText());
-//                        //Log.d("tag","secret "+result.getText());
-//                        Intent intent = new Intent(getApplicationContext(),LockActivity.class);
-//                        startActivity(intent);
-//                    }
-//                });
-//            }
-//        });
-//
-//        scannerView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openCamera();
-//            }
-//        });
+        buttonOpenScanner = findViewById(R.id.btnOpenScanner);
+        buttonOpenScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanCode();
+            }
+        });
     }
 
-//    @SuppressLint("ObsoleteSdkInt")
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if (checkSelfPermission(Manifest.permission.CAMERA) ==
-//                    PackageManager.PERMISSION_DENIED ||
-//                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-//                            PackageManager.PERMISSION_DENIED) {
-//                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-//                requestPermissions(permission, PERMISSION_CODE);
-//            } else {
-//                openCamera();
-//            }
-//        } else {
-//            openCamera();
-//        }
-//    }
-//    public void openCamera() {
-//        codeScanner.startPreview();
-//    }
+    private void scanCode(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to Flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() !=null ){
+            AlertDialog.Builder builder = new AlertDialog.Builder(Scanner.this);
+            builder.setTitle("Scan Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            final AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+    });
 }
